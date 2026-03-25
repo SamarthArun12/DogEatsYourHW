3d58ea1eae346f50810ee67fa02e48aad01c60c5
Samarth Arun <samartharun@AmbikaM1-MacBook-Pro.local>
2026-03-24 17:27:33 -0700

    image processing

--- main.py
+++ main.py
@@ -28,21 +28,22 @@ def gemini_check():
 
     #keeps on asking gemini until returned a valid response
     while True:
-        #next 7 lines of code ai generated (prompt made by me however)
+        #this section which actually interacts with api is ai generated
         response = client.models.generate_content(
-    model="gemini-3.1-flash-lite-preview",
-    contents=[
-            types.Part.from_bytes(data=image_data, mime_type="image/jpeg"),
-            #cool detailed prompt for increased consistency and accuracy, also gives gemini previous response which errored.
-            "Step 1: Count number of questions in this image (look for numbered problems (use ex:1, 2, 3, 1a, 1b, 1c etc when counting)). Step 2: Then estimate number of minutes taken per question. Guidline Time/Q (approximate, DO NOT STRICTLY FOLLOW): simple math problem: 0.5-0.75 mins, moderate math problem: 2-3 mins, hard math problem: 4-5 mins. 1 page of history notes ~10 mins. Use your judgement for difficulty. Step 3: return total time taken as an integer. Fix error from previous response if there is one. IMPORTANT: Respond with ONLY a whole number. NO UNITS, WORDS, SYMBOLS. " + errors
-        ]   ,
-    config=types.GenerateContentConfig(
-        response_mime_type="application/json",
-        response_schema={"type": "INTEGER"}
-    )
-)
+            model="gemini-3.1-flash-lite-preview",
+            contents=[
+                    types.Part.from_bytes(data=image_data, mime_type="image/jpeg"),
+                    #detailed prompt made by me for accuracy. Also returns previous mistake if one made
+                    "Step 1: Count number of questions in this image (look for numbered problems (use ex:1, 2, 3, 1a, 1b, 1c etc when counting)). Step 2: Then estimate number of minutes taken per question. Guidline Time/Q (approximate, DO NOT STRICTLY FOLLOW): simple math problem: 0.5-0.75 mins, moderate math problem: 2-3 mins, hard math problem: 4-5 mins. 1 page of history notes ~10 mins. Use your judgement for difficulty. Step 3: return total time taken as an integer. Fix error from previous response if there is one. IMPORTANT: Respond with ONLY a whole number. NO UNITS, WORDS, SYMBOLS. " + errors
+                ]   ,
+            #makes it so that only an integer can be sent back
+            config=types.GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema={"type": "INTEGER"}
+            )
+        )
 
-        #if gemini's response isn't an integer, try again
+        #double check to make sure gemini returned an integer
         if response.text.isdigit():
             minutes = int(response.text)
             break
@@ -50,43 +51,50 @@ def gemini_check():
             #errors is given to gemini so it can see what was wrong with a previous response
             errors = "Prev response couldn't be converted to integer. Prev response which is invalid: " + response.text
             print(response.text)
+
     return minutes
-
-
 
 pygame.init()
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
 running = True
 
+#all the buttons and the coordinates are in a list so code is more compact
+buttons = {
+    "picture": (15, 410, 177.5, 75),
+    "stop": (207.5, 410, 177.5, 75),
+    "pause": (15, 320, 177.5, 75),
+    "button4": (207.5, 320, 177.5, 75),
+    "image_loc": (15, 55, 370, 250),
+}
+#rects used later in mousebuttondown for click detection
+rects = {}
+#draws rectangles
+for key, coords in buttons.items():
+    rect = pygame.Rect(coords)
+    rects[key] = rect
+for key, rectangle in rects.items():
+    pygame.draw.rect(screen, (200,200,200), rectangle)
+
 hunger = 100
-
-#just placeholder names btw
-picture_button = pygame.Rect(15, 410, 177.5, 75)
-pygame.draw.rect(screen, (200, 200, 200), picture_button)
-
-#bottom right
-button2 = pygame.Rect(207.5, 410, 177.5, 75)
-pygame.draw.rect(screen, (200, 200, 200), button2)
-
-#top left
-button3 = pygame.Rect(15, 320, 177.5, 75)
-pygame.draw.rect(screen, (200, 200, 200), button3)
-
-#top right
-button4 = pygame.Rect(207.5, 320, 177.5, 75)
-pygame.draw.rect(screen, (200, 200, 200), button4)
-
-#where the image shud be
-image_loc = pygame.Rect(15, 55, 370, 250)
-pygame.draw.rect(screen, (200, 200, 200), image_loc)
 
 while running:
     #required for window to not freeze
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            click_location = event.pos
+            action = None
+            for key, rectangle in rects.items():
+                if rectangle.collidepoint(click_location):
+                    action = key
+                    break
+            #skip rest of code if empty space was clicked
+            if action == None: continue
+            print("button "+key+" clicked!")
 
-    #no acc functionality as of yet js displays the rects
+
+    #actually displays pictures and updated stuff
     pygame.display.flip()
     clock.tick(15)
